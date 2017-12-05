ed7fb699c2299dba3008635e5cef2819c7b2faa7
Olga Shiryaeva <shiryaeva@haulmont.com>
2017-12-05 16:42:42 +0400

    init

---
+++ modules/global/src/com/company/sales/entity/OrderLine.java
@@ -0,0 +1,53 @@
+package com.company.sales.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+@Table(name = "SALES_ORDER_LINE")
+@Entity(name = "sales$OrderLine")
+public class OrderLine extends StandardEntity {
+    private static final long serialVersionUID = -2202863497466101774L;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ORDER_ID")
+    protected Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PRODUCT_ID")
+    protected Product product;
+
+    @Column(name = "QUANTITY", precision = 19, scale = 3)
+    protected BigDecimal quantity;
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+
+}
